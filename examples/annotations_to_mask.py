92ebd51a91f56926be1a7f2fd29f3bbafa167c2b
Jonas Teuwen <2347927+jonasteuwen@users.noreply.github.com>
2024-09-26 17:32:12 +0200

    Use dynamic loading for annotations (#255)
    - Use dynamic loading for annotation reading and writing

--- examples/annotations_to_mask.py
+++ examples/annotations_to_mask.py
@@ -1,16 +1,16 @@
 # Copyright (c) dlup contributors
+# mypy: disable-error-code="attr-defined"
 """This code provides an example of how to convert annotations to a mask."""
 from pathlib import Path
 
 import PIL.Image
 
-from dlup.annotations_experimental import SlideAnnotations
+from dlup.annotations import SlideAnnotations
 
 
 def convert_annotations_to_mask() -> None:
     scaling = 0.02
     annotations = SlideAnnotations.from_dlup_xml(Path(__file__).parent / "files" / "dlup_annotation_test.xml")
-
     bbox = annotations.bounding_box_at_scaling(scaling)
 
     region = annotations.read_region((0, 0), scaling, bbox[1])
